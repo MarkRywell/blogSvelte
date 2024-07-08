17324a197a0c464e214ed07e40d9961d9e25674f
MarkRywell <gopro855@gmail.com>
2024-07-08 21:17:39 +0800

    Initial commit

---
+++ src/lib/components/input.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let type = 'text';
+    export let placeholder = '';
+    export let required = false;
+</script>
+
+<input type={type} placeholder={placeholder} required={required} class="min-w-full h-full bg-gray-100 rounded-full pl-5 focus:outline-none focus:ring-0" />
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+    <title>Login</title>
+    <meta name="description" content="Login page"> 
+</svelte:head>
+
+<script>
+    import Input from "$lib/components/input.svelte";
+</script>
+
+<main class="h-screen w-screen flex flex-row flex-wrap">
+    <img src="/intro-bg1.jpg" alt="Login" class="h-full w-1/2 object-cover">
+    <div class="w-1/2 flex flex-col justify-center items-center text-center p-60">
+        <h1 class="text-3xl text-center mb-8 font-semibold">Sign in to Blogosphere</h1>
+        <div class="w-full h-[40px] mb-5">
+            <Input type="email" placeholder="Email" />
+        </div>
+        <div class="w-full h-[40px] mb-5">
+            <Input type="password" placeholder="Password" />
+        </div>
+    </div>
+</main>
