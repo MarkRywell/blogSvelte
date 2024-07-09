537cce1c9a446ba61c2bf63a45e9f8fffc6c2e42
MarkRywell <gopro855@gmail.com>
2024-07-09 13:13:03 +0800

    Add input component

--- src/lib/components/input.svelte
+++ src/lib/components/input.svelte
@@ -1,7 +1,20 @@
-<script>
+<script lang="ts">
     export let type = 'text';
     export let placeholder = '';
     export let required = false;
-</script>
-
-<input type={type} placeholder={placeholder} required={required} class="min-w-full h-full bg-gray-100 rounded-full pl-5 focus:outline-none focus:ring-0" />
+    export let value = '';
+  
+    function handleInput(event: any) {
+      value = event.target.value;
+    }
+  </script>
+  
+  <input 
+    type={type} 
+    placeholder={placeholder} 
+    required={required} 
+    value={value} 
+    on:input={handleInput} 
+    class="min-w-full h-full bg-gray-100 rounded-full pl-5 focus:outline-none focus:ring-0" 
+  />
+  
